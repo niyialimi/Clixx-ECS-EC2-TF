0ceb15a2cfb45ff82595984a30433cd7d6827a99
Niyi Alimi <neyonill@yahoo.com>
2022-06-30 09:25:03 +1000

    Added Route53 and Zone ID

--- route53.tf
+++ route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "clixx-app" {
   zone_id = var.zone_id
   name    = "ecs.dev.clixx-niyialimi.com"
-  type    = "A"
+  type    = "CNAME"
   ttl     = "300"
   records = [aws_lb.clixx-ALB-ECS-tf.dns_name]
 }
@@ -17,4 +17,13 @@ resource "aws_route53_record" "clixx-app" {
 #     name                   = aws_lb.clixx-ALB-ECS-tf.dns_name
 #     #zone_id                = aws_lb.example.zone_id
 #   }
+# }
+
+
+# resource "aws_route53_record" "dub" {
+#   zone_id = "Z18WO5VD3QY05U"
+#   name    = "dub.isochron.us"
+#   type    = "CNAME"
+#   ttl     = "6"
+#   records = ["cname.backplane.io"]
 # }
