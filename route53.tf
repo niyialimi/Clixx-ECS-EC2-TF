5d11ac0893e51114eb87e27027648f84efa9c2ed
Niyi Alimi <neyonill@yahoo.com>
2022-06-30 09:10:54 +1000

    Added Route53 and Zone ID

--- route53.tf
+++ route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "clixx-app" {
   zone_id = var.zone_id
   name    = "dev.clixx-niyialimi.com"
-  type    = "CNAME"
+  type    = "A"
   ttl     = "300"
   records = [aws_lb.clixx-ALB-ECS-tf.dns_name]
 }
