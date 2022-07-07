46ddd47aea9bcbafc21515e7e18066091d1dc174
Niyi Alimi <neyonill@yahoo.com>
2022-07-07 13:28:17 +1000

    Changed domain name

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "clixx-app" {
   zone_id = var.zone_id
-  name    = "ecs.dev.clixx-niyialimi.com"
+  name    = "ecs.uat.clixx-niyialimi.com"
   type    = "CNAME"
   ttl     = "300"
   records = [aws_lb.clixx-ALB-ECS-tf.dns_name]
